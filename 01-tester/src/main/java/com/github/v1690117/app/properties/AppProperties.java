7250a3f9bd4a5992bf476beb78ed4ff089468543
v1690117 <vladimir.veselov@phhystech.edu>
2020-03-22 19:41:55 +0300

    Small fixes

--- 01-tester/src/main/java/com/github/v1690117/app/properties/AppProperties.java
+++ 01-tester/src/main/java/com/github/v1690117/app/properties/AppProperties.java
@@ -1,12 +1,10 @@
 package com.github.v1690117.app.properties;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-@Getter
-@Setter
+@Data
 @Component
 @ConfigurationProperties("application")
 public class AppProperties {
