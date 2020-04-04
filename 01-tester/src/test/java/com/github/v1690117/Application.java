2900a05e2f6203b5a9aa606d4afbe9ab6f89d56b
v1690117 <vladimir.veselov@phhystech.edu>
2020-04-04 21:08:29 +0300

    Tests were fixed.

--- 01-tester/src/test/java/com/github/v1690117/Application.java
+++ 01-tester/src/test/java/com/github/v1690117/Application.java
@@ -4,29 +4,17 @@ import com.github.v1690117.app.properties.AppProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.context.i18n.LocaleContextHolder;
-
-import java.util.Locale;
 
 @Configuration
 @ComponentScan({
         "com.github.v1690117.app.poll",
         "com.github.v1690117.app.util"
 })
-@Profile("test")
 public class Application {
-
-    // todo understand why poperties are not injected
     @Bean
     public AppProperties properties() {
         AppProperties properties = new AppProperties();
         properties.setFilename("test-questions.csv");
         return properties;
-    }
-
-    @Bean
-    public Locale locale() {
-        return LocaleContextHolder.getLocale();
     }
 }
