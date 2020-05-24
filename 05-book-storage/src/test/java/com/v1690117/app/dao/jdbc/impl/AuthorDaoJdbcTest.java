445320173af7b6810d875e63dea347a6f32e2785
v1690117 <vladimir.veselov@phhystech.edu>
2020-05-24 18:54:13 +0300

    Test for authors DAO was added

--- 05-book-storage/src/test/java/com/v1690117/app/dao/jdbc/impl/AuthorDaoJdbcTest.java
+++ 05-book-storage/src/test/java/com/v1690117/app/dao/jdbc/impl/AuthorDaoJdbcTest.java
@@ -1,6 +1,7 @@
-package com.v1690117.app.dao;
+package com.v1690117.app.dao.jdbc.impl;
 
-import com.v1690117.app.model.Genre;
+import com.v1690117.app.dao.AuthorDao;
+import com.v1690117.app.model.Author;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -12,59 +13,64 @@ import org.springframework.test.context.junit4.SpringRunner;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-@DisplayName("Genres DAO")
+@DisplayName("Authors DAO")
 @RunWith(SpringRunner.class)
 @JdbcTest
 @ComponentScan("com.v1690117.app.dao")
-class GenreDaoTest {
+class AuthorDaoJdbcTest {
     @Autowired
-    private GenreDao dao;
+    private AuthorDao dao;
 
     @DisplayName("Counts entities")
     @Test
     void count() {
-        assertThat(dao.count()).isEqualTo(9);
+        assertThat(dao.count()).isEqualTo(23);
     }
 
     @DisplayName("Finds entity by id")
     @Test
     void findById() {
-        Genre expected = new Genre(1, "drama");
+        Author expected = new Author(1, "Martin", "Fowler");
         assertThat(dao.findById(1)).isEqualToComparingFieldByField(expected);
     }
 
     @DisplayName("Finds all entities")
     @Test
     void findAll() {
-        Genre expected = new Genre(1, "drama");
+        Author mFowler = new Author(1, "Martin", "Fowler");
+        Author rMartin = new Author(10, "Robert", "Martin");
         assertThat(dao.findAll())
-                .hasSize(9)
-                .contains(expected);
+                .hasSize(23)
+                .contains(mFowler)
+                .contains(rMartin);
     }
 
     @DisplayName("Adds new entity")
     @Test
     void insert() {
-        Genre expected = new Genre(10, "anything");
+        Author expected = new Author(24, "Vladimir", "Veselov");
         dao.insert(expected);
         assertThat(dao.findById(expected.getId())).isEqualToComparingFieldByField(expected);
+        assertThat(dao.count()).isEqualTo(24);
     }
 
     @DisplayName("Updates entity")
     @Test
     void update() {
-        Genre first = dao.findById(1);
-        assertThat(first.getName()).isEqualTo("drama");
-        dao.update(new Genre(first.getId(), "melodrama"));
-        first = dao.findById(1);
-        assertThat(first.getName()).isEqualTo("melodrama");
+        Author expected = new Author(24, "Vladimir", "Veselov");
+        dao.insert(expected);
+        dao.update(new Author(24, "Alexander", "Veselov"));
+        Author updated = dao.findById(24);
+        assertThat(updated)
+                .extracting(Author::getFirstName, Author::getLastName)
+                .containsExactly("Alexander", "Veselov");
     }
 
     @DisplayName("Can't delete entity because of relations")
     @Test
     void delete() {
         assertThatThrownBy(() -> {
-            dao.delete(9);
+            dao.delete(1);
         }).hasMessageContaining("constraint violation");
     }
 }
