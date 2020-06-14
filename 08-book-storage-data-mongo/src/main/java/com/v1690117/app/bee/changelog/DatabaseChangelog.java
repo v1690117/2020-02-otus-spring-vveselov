cd52eda721e9e0f784fabca9efb0bcebd5e30b30
v1690117 <vladimir.veselov@phhystech.edu>
2020-06-14 23:48:23 +0300

    Sample data from previous task was added

--- 08-book-storage-data-mongo/src/main/java/com/v1690117/app/bee/changelog/DatabaseChangelog.java
+++ 08-book-storage-data-mongo/src/main/java/com/v1690117/app/bee/changelog/DatabaseChangelog.java
@@ -1,57 +1,68 @@
 package com.v1690117.app.bee.changelog;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.cloudyrock.mongock.ChangeLog;
 import com.github.cloudyrock.mongock.ChangeSet;
 import com.v1690117.app.model.Author;
 import com.v1690117.app.model.Book;
+import com.v1690117.app.model.DatabaseSequence;
 import com.v1690117.app.model.Genre;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.SneakyThrows;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
-import java.util.Collections;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 @ChangeLog
+@AllArgsConstructor
 public class DatabaseChangelog {
 
-    @ChangeSet(order = "001", id = "addGenres", author = "v1690117")
-    public void insertGenres(MongoTemplate mt) {
-        mt.save(new Genre(1L, "drama"));
-        mt.save(new Genre(2L, "comedy"));
-        mt.save(new Genre(3L, "science"));
-        mt.save(new Genre(3L, "scifi"));
-        mt.save(new Genre(8L, "software development"));
-    }
-
-    @ChangeSet(order = "002", id = "addAuthors", author = "v1690117")
-    public void insertAuthors(MongoTemplate mt) {
+    @SneakyThrows
+    @ChangeSet(order = "001", id = "addSamples", author = "v1690117")
+    public void insertSampleData(MongoTemplate mt) {
+        SampleData samples = new ObjectMapper().readValue(
+                new InputStreamReader(
+                        this.getClass()
+                                .getClassLoader()
+                                .getResourceAsStream("data.json"), // todo move to app props
+                        StandardCharsets.UTF_8
+                ),
+                SampleData.class
+        );
+        samples.getGenres().forEach(genre -> mt.save(genre));
+        samples.getAuthors().forEach(author -> mt.save(author));
+        samples.getBooks().forEach(book -> mt.save(book));
         mt.save(
-                new Author(
-                        2L,
-                        "Egor",
-                        "Bugaenko"
+                new DatabaseSequence(
+                        Book.SEQUENCE_NAME,
+                        samples.getBooks().size()
+                )
+        );
+        mt.save(
+                new DatabaseSequence(
+                        Genre.SEQUENCE_NAME,
+                        samples.getGenres().size()
+                )
+        );
+        mt.save(
+                new DatabaseSequence(
+                        Author.SEQUENCE_NAME,
+                        samples.getAuthors().size()
                 )
         );
     }
 
-    @ChangeSet(order = "003", id = "addBooks", author = "v1690117")
-    public void insertBooks(MongoTemplate mt) {
-        mt.save(
-                new Book(
-                        1L,
-                        "Elegant Objects",
-                        "... Elegant Objects ...",
-                        "2016",
-                        Collections.singletonList(
-                                new Author(
-                                        2L,
-                                        "Egor",
-                                        "Bugaenko"
-                                )
-                        ),
-                        Collections.singletonList(
-                                new Genre(8L, "software development")
-                        ),
-                        Collections.emptyList()
-                )
-        );
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    static class SampleData {
+        private List<Genre> genres;
+        private List<Author> authors;
+        private List<Book> books;
     }
 }
--- 08-book-storage-data-mongo/src/main/java/com/v1690117/app/model/DatabaseSequence.java
+++ 08-book-storage-data-mongo/src/main/java/com/v1690117/app/model/DatabaseSequence.java
@@ -1,10 +1,12 @@
 package com.v1690117.app.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@AllArgsConstructor
 @Document(collection = "database_sequences")
 public class DatabaseSequence {
     @Id
