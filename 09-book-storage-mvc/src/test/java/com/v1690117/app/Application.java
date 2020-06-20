95e2a482beed4f7e9d6d687446bb3800e0ae0605
v1690117 <vladimir.veselov@phhystech.edu>
2020-06-20 17:17:15 +0300

    Module 9 was inited

---
+++ 09-book-storage-mvc/src/test/java/com/v1690117/app/Application.java
@@ -0,0 +1,11 @@
+package com.v1690117.app;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({
+        "com.v1690117.app"
+})
+public class Application {
+}
